d450055947e92954ac8cba0526cabe6367a18a65
Zeynep Ceyhan <104215922+zeynepnceyhan@users.noreply.github.com>
2022-08-01 13:32:17 +0300

    For loop
    internship

---
+++ ForLoop/ForLoop/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("count 1 to 100:");
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Finished!************************************************************");
+            Console.WriteLine("count 1 to 100(only odd numbers):");
+
+            for (int i = 1; i <= 100; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Finished!*************************************************************");
+            Console.WriteLine("count 0 to 100(only even numbers):");
+            for (int i = 0; i <= 100; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("count 100 to 1:");
+            for (int i = 100; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Program has already finished!!!!");
+            Console.ReadLine();
+
+        }
+
+
+    }
+}
